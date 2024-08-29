2aa8eb152f5009cecd1079c6258b9184f36611f1
Sabariah <sabariahsaifulrahim5146@gmail.com>
2024-08-29 11:30:02 +0800

    API

---
+++ e-systemApplicationAPI/e-systemApplicationAPI/Controllers/ApplicationsController.cs
@@ -0,0 +1,113 @@
+﻿using e_systemApplicationAPI.Data;
+using e_systemApplicationAPI.IRepository;
+using e_systemApplicationAPI.Models;
+using MailKit.Security;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MimeKit.Text;
+using MimeKit;
+using MailKit.Net.Smtp;
+using e_systemApplicationAPI.Repository;
+
+namespace e_systemApplicationAPI.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class ApplicationsController : ControllerBase
+    {
+        private readonly IGenericRepository<Application> _genericRepository;
+        private readonly ApplicationDbContext _context;
+        private readonly EmailRepository _emailRepository;
+
+
+        public ApplicationsController(IGenericRepository<Application> genericRepository, ApplicationDbContext context, EmailRepository emailRepository)
+        {
+            _genericRepository = genericRepository;
+            _context = context;
+            _emailRepository = emailRepository;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddApplication([FromBody] Application application)
+        {
+            application.CreatedDate = DateTime.Now;
+            var addedApplication = await _genericRepository.Add(application);
+            return Ok(addedApplication);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetApplication()
+        {
+            var applications = await _genericRepository.GetAll();
+            return Ok(applications);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> ChangeStatus([FromBody] int id)
+        {
+            var existingApplication = await _context.Applications.FirstOrDefaultAsync(x => x.Id == id);
+            if (existingApplication != null)
+            {
+                existingApplication.Status = "Cancel";
+                await _context.SaveChangesAsync();
+            }
+            return Ok(existingApplication);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> ApprovedApplication([FromBody] Application[] applications )
+        {
+            foreach(var item in applications)
+            {
+                var existingApplication = await _context.Applications.FirstOrDefaultAsync(x=>x.Id == item.Id);
+                if(existingApplication != null)
+                {
+                    existingApplication.ApprovalId = item.ApprovalId;
+                    existingApplication.ApprovalDate = DateTime.Now;
+                    existingApplication.Status = item.Status;
+                    await _context.SaveChangesAsync();
+                }
+            }
+            return Ok(applications);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> AssignedApplication([FromBody] Application[] applications)
+        {
+            foreach (var item in applications)
+            {
+                var existingApplication = await _context.Applications.FirstOrDefaultAsync(x => x.Id == item.Id);
+                if (existingApplication != null)
+                {
+                    existingApplication.AssignedBy = item.AssignedBy;
+                    existingApplication.AssignedDate = DateTime.Now;
+                    existingApplication.Status = item.Status;
+                    await _context.SaveChangesAsync();
+                    await _emailRepository.sendWelcomeEmail(existingApplication);
+                }
+            }
+            return Ok(applications);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> RejectApplication([FromBody] Application[] applications)
+        {
+            foreach (var item in applications)
+            {
+                var existingApplication = await _context.Applications.FirstOrDefaultAsync(x => x.Id == item.Id);
+                if (existingApplication != null)
+                {
+                    existingApplication.ApprovalId = item.ApprovalId;
+                    existingApplication.ApprovalDate = DateTime.Now;
+                    existingApplication.Status = item.Status;
+                    await _context.SaveChangesAsync();
+                    await _emailRepository.sendRejectEmail(existingApplication);
+                }
+            }
+            return Ok(applications);
+        }
+
+
+    }
+}
---
+++ e-systemApplicationAPI/e-systemApplicationAPI/Models/System.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace e_systemApplicationAPI.Models
+{
+    public class System
+    {
+        [Key]
+        public int Id { get; set; }
+        public string SystemName { get; set; }
+        public bool IsActive { get; set; }
+        public string? CreatedBy { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public string? UpdatedBy { get; set; }
+        public DateTime UpdatedDate { get; set; }
+    }
+}
