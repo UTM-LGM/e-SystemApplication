767ce2f20f6676faab697aaebb6044bd2e79773e
Sabariah <sabariahsaifulrahim5146@gmail.com>
2024-08-29 11:26:40 +0800

    clear

--- e-systemApplication/src/app/_interface/system.ts
+++
@@ -1,9 +0,0 @@
-export interface System{
-    id:number
-    systemName:string
-    isActive:boolean
-    createdBy:string
-    createdDate:Date
-    updatedBy:string
-    updatedDate:Date
-}
--- e-systemApplication/src/environments/environment.prod.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-    production: true,
-    apiUrl: 'https://lgm20.lgm.gov.my/e-EstateApi/api'
-  };
